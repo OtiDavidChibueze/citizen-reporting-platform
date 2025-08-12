366bec0c165b54b4aba724727ffe1b346d8f012c
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 00:56:14 +0100

    feat(routes): configure splash scrren and register page route

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,7 +1,22 @@
+import 'package:citizen_report_incident/features/incidents/presentation/pages/register_page.dart';
+import 'package:citizen_report_incident/features/splash/presentation/page/splash.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoutes {
-  static final GoRouter goRouter = GoRouter(routes: [
+  static final GoRouter goRouter = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        name: Splash.routeName,
+        builder: (context, state) => Splash(),
+      ),
+
+      GoRoute(
+        path: '/register',
+        name: RegisterPage.routeName,
+        builder: (context, state) => RegisterPage(),
+      ),
     ],
   );
 }
