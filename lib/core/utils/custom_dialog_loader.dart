083850cffa68a4fc9107cd6507fc36c43cc83370
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 00:47:22 +0100

    feat(helper): created an app wide dialog loader and snackbar

---
+++ lib/core/utils/custom_dialog_loader.dart
@@ -0,0 +1,37 @@
+import 'package:citizen_report_incident/core/common/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class CustomDialogLoader {
+  static bool _isDialogOpen = false;
+
+  static void show(BuildContext context) {
+    if (!_isDialogOpen) {
+      _isDialogOpen = true;
+    }
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (_) {
+          return PopScope(
+            canPop: false,
+            child: Center(
+              child: SpinKitCubeGrid(color: AppColors.white, size: 50),
+            ),
+          );
+        },
+      ).then((value) => _isDialogOpen = false);
+    });
+  }
+
+  static void cancel(BuildContext context) {
+    if (_isDialogOpen) {
+      _isDialogOpen = false;
+      Navigator.of(context, rootNavigator: true).pop();
+    }
+  }
+
+  static void isDialogOpenCurrently() => _isDialogOpen;
+}
---
+++ lib/core/utils/custom_snackbar.dart
@@ -0,0 +1,24 @@
+import 'package:citizen_report_incident/core/common/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomSnackbar {
+  static void success(BuildContext context, String message) {
+    _snackBar(context, message, AppColors.successSnackbar);
+  }
+
+  static void error(BuildContext context, String message) {
+    _snackBar(context, message, AppColors.error);
+  }
+
+  static void info(BuildContext context, String message) {
+    _snackBar(context, message, AppColors.infoSnackbar);
+  }
+
+  static _snackBar(BuildContext context, String message, Color color) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(message), backgroundColor: color));
+    });
+  }
+}
