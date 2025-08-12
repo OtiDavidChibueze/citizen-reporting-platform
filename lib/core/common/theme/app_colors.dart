b8e722e8ce73e32c4d949776e2c1aaa517e114ca
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 00:51:44 +0100

    style: add new colors

---
+++ lib/core/common/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color successSnackbar = Colors.green;
+  static const Color error = Colors.red;
+  static const Color infoSnackbar = Colors.blue;
+  static const Color white = Colors.white;
+  static const Color scaffold = Colors.black;
+  static final Color borderColor = Colors.grey;
+}
