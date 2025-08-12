20365a7e64d1f1d33aa0c8314c5f0242554c0b13
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 00:42:16 +0100

    feat(splash): implemented splash screen

---
+++ lib/features/splash/presentation/page/splash.dart
@@ -0,0 +1,51 @@
+import 'package:citizen_report_incident/core/common/theme/app_colors.dart';
+import 'package:citizen_report_incident/core/constants/app_image_urls.dart';
+import 'package:citizen_report_incident/core/utils/screen_util.dart';
+import 'package:citizen_report_incident/features/incidents/presentation/pages/register_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Splash extends StatefulWidget {
+  static const routeName = 'splash';
+
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      context.goNamed(RegisterPage.routeName);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffold,
+      body: Center(
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(color: AppColors.white, blurRadius: 30),
+                BoxShadow(color: AppColors.white, blurRadius: 15),
+              ],
+            ),
+            child: Image.asset(
+              AppImageUrls.logo,
+              fit: BoxFit.cover,
+              height: h(200),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
