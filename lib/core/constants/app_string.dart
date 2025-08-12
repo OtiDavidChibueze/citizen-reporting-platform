6d46137380120c999b6a4b749cb37b765036e98a
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 00:54:18 +0100

    added more strings

--- lib/core/constants/app_string.dart
+++ lib/core/constants/app_string.dart
@@ -2,4 +2,15 @@ class AppString {
   static const String appName = 'Bincom Citizen Report';
   static const String failure =
       'Sorry an unexpected error occured. Please try again later';
+  static const String noInternetConnection = 'Please connect to the internet';
+  static const String registerSuccess = 'Registered Account, please login!';
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String required = 'Field is required!';
+  static const String emailValidation = 'Please enter a valid email address!';
+  static const String passwordValidation =
+      'Password must be between 6 and 20 characters!';
+  static const String register = 'Register';
+  static const String alreadyHaveAccount = 'Already have an account? ';
+  static const String login = 'Login';
 }
