65600cde06e60432b846491d534071fec9627a8a
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 05:34:58 +0100

    fix(register): fix return value

--- lib/features/auth/domain/usecases/register_usecase.dart
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -1,17 +1,19 @@
+import '../entities/user_entity.dart';
+
 import '../../../../core/error/failure.dart';
 import '../../../../core/usecases/usecases.dart';
 import '../../data/dto/register_dto.dart';
 import '../repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
-class RegisterUseCase implements UseCase<dynamic, RegisterDTO> {
+class RegisterUseCase implements UseCase<UserEntity, RegisterDTO> {
   final AuthRepository _authRepository;
 
   RegisterUseCase({required AuthRepository authRepository})
     : _authRepository = authRepository;
 
   @override
-  Future<Either<Failure, dynamic>> call(RegisterDTO params) async {
+  Future<Either<Failure, UserEntity>> call(RegisterDTO params) async {
     return _authRepository.register(params);
   }
 }
