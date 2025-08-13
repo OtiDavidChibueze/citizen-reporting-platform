b1f5584c66418d405aaf1eb2e00d6b8364e6d2d6
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 01:11:12 +0100

    refactor(auth): register register user flow, internet connect checker and uuid

--- lib/core/service/locator_service.dart
+++ lib/core/service/locator_service.dart
@@ -1,8 +1,34 @@
 import 'package:citizen_report_incident/core/service/firebase_service.dart';
+import 'package:citizen_report_incident/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:citizen_report_incident/features/auth/data/source/remote/auth_firebase_remote_source.dart';
+import 'package:citizen_report_incident/features/auth/domain/repository/auth_repository.dart';
+import 'package:citizen_report_incident/features/auth/domain/usecases/register_usecase.dart';
+import 'package:citizen_report_incident/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:uuid/uuid.dart';
 
 final locatorService = GetIt.I;
 
 void setupLocator() {
   locatorService.registerLazySingleton(() => FirebaseService());
+  locatorService.registerLazySingleton(() => Uuid());
+  locatorService.registerLazySingleton(() => InternetConnectionChecker.I);
+
+  _initAuth();
+}
+
+_initAuth() {
+  locatorService
+    ..registerLazySingleton<AuthFirebaseRemoteSource>(
+      () => AuthFirebaseRemoteSourceImpl(
+        firebaseService: locatorService(),
+        internetConnectionChecker: locatorService(),
+      ),
+    )
+    ..registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(authFirebaseRemoteSource: locatorService()),
+    )
+    ..registerFactory(() => RegisterUseCase(authRepository: locatorService()))
+    ..registerLazySingleton(() => AuthBloc(registerUseCase: locatorService()));
 }
