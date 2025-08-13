754f7f230b0e35d6aea6088e2a6b45dc753c7877
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 01:54:30 +0100

    feat(auth): create user entity and user model

---
+++ lib/features/auth/data/model/user_model.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:citizen_report_incident/features/auth/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required super.name,
+    required super.email,
+    required super.password,
+  });
+
+  UserModel copyWith({String? name, String? email, String? password}) {
+    return UserModel(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() =>
+      'UserModel(name: $name, email: $email, password: $password)';
+
+  @override
+  bool operator ==(covariant UserModel other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name &&
+        other.email == email &&
+        other.password == password;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ email.hashCode ^ password.hashCode;
+}
