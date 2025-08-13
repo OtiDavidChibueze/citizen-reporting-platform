70d9bee481b9684ca32f018621015bc794730d89
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-13 01:09:44 +0100

    refactor: configure authbloc, and set up system overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:citizen_report_incident/app.dart';
 import 'package:citizen_report_incident/core/service/firebase_service.dart';
 import 'package:citizen_report_incident/core/service/locator_service.dart';
+import 'package:citizen_report_incident/features/auth/presentation/bloc/auth_bloc.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
 
   await FirebaseService.init();
 
   setupLocator();
 
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => locatorService<AuthBloc>())],
+      child: const MyApp(),
+    ),
+  );
 }
