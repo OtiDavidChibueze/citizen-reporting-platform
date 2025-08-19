91136e64728f5c958dc759b4ccb8835c7071f0b3
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-19 01:29:11 +0100

    feat: implement incident display in IncidentView and enhance HomePage with refresh functionality

--- lib/features/incidents/presentation/pages/home_page.dart
+++ lib/features/incidents/presentation/pages/home_page.dart
@@ -1,101 +1,3 @@
-// import 'package:citizen_report_incident/features/auth/domain/entities/user_entity.dart';
-
-// import '../../../../core/utils/screen_util.dart';
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import '../../../../core/service/local_storage_service.dart';
-// import '../../../../core/storage/app_storage_keys.dart';
-
-// class HomePage extends StatelessWidget {
-//   static const String routeName = 'home';
-
-//   final UserEntity? currentUser;
-
-//   const HomePage({super.key, this.currentUser});
-
-//   String? get currentUserId => LocalStorageService().get(AppStorageKeys.uid);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final userId = currentUserId;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           'Welcome back ${currentUser?.email ?? 'User'}',
-//           style: TextStyle(fontWeight: FontWeight.bold, fontSize: sp(15)),
-//         ),
-//         centerTitle: true,
-//       ),
-//       body: userId == null
-//           ? const Center(child: Text('User not logged in.'))
-//           : StreamBuilder<QuerySnapshot>(
-//               stream: FirebaseFirestore.instance
-//                   .collection('incidents')
-//                   .where('createdByUserId', isEqualTo: userId)
-//                   .orderBy('createdAt', descending: true)
-//                   .snapshots(),
-//               builder: (context, snapshot) {
-//                 if (snapshot.connectionState == ConnectionState.waiting) {
-//                   return const Center(child: CircularProgressIndicator());
-//                 }
-//                 if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-//                   return Center(
-//                     child: Text(
-//                       'You have\'nt uploaded any incidents yet.',
-//                       style: TextStyle(fontSize: sp(16)),
-//                     ),
-//                   );
-//                 }
-//                 final docs = snapshot.data!.docs;
-//                 return ListView.builder(
-//                   itemCount: docs.length,
-//                   itemBuilder: (context, idx) {
-//                     final data = docs[idx].data() as Map<String, dynamic>;
-//                     final imageUrl = data['imageUrl'] ?? '';
-//                     Widget leadingWidget;
-//                     if (imageUrl is String && imageUrl.startsWith('http')) {
-//                       leadingWidget = SizedBox(
-//                         width: 60,
-//                         height: 60,
-//                         child: Image.network(imageUrl, fit: BoxFit.cover),
-//                       );
-//                     } else {
-//                       leadingWidget = const SizedBox(
-//                         width: 60,
-//                         height: 60,
-//                         child: Icon(Icons.image_not_supported),
-//                       );
-//                     }
-//                     return Card(
-//                       margin: const EdgeInsets.symmetric(
-//                         horizontal: 12,
-//                         vertical: 8,
-//                       ),
-//                       child: ListTile(
-//                         leading: leadingWidget,
-//                         title: Text(data['title'] ?? ''),
-//                         subtitle: Column(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             Text('Category: ${data['category'] ?? ''}'),
-//                             Text('Description: ${data['description'] ?? ''}'),
-//                             Text(
-//                               'Location: (${data['latitude'] ?? ''}, ${data['longitude'] ?? ''})',
-//                             ),
-//                             Text('By: ${data['createdByEmail'] ?? ''}'),
-//                           ],
-//                         ),
-//                         isThreeLine: true,
-//                       ),
-//                     );
-//                   },
-//                 );
-//               },
-//             ),
-//     );
-//   }
-// }
-
 import 'package:citizen_report_incident/core/common/theme/app_colors.dart';
 import 'package:citizen_report_incident/core/utils/custom_snackbar.dart';
 import 'package:citizen_report_incident/core/utils/screen_util.dart';
@@ -138,92 +40,99 @@ class _HomePageState extends State<HomePage> {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: w(16), vertical: h(16)),
       child: RefreshIndicator(
+        elevation: 0,
+        color: AppColors.scaffold,
+        backgroundColor: AppColors.white,
         onRefresh: () {
           context.read<IncidentBloc>().add(GetIncidentsEvent());
 
           return Future.value();
         },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Welcome back ',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: sp(13),
+        child: Scrollbar(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome back ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: sp(13),
+                        ),
                       ),
-                    ),
 
-                    Text(
-                      '${widget.currentUser?.fullname}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: sp(23),
+                      Text(
+                        '${widget.currentUser?.fullname}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: sp(23),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-
-                IconButton.outlined(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+                    ],
                   ),
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.notifications,
-                    size: 30,
-                    color: AppColors.scaffold,
-                  ),
-                ),
-              ],
-            ),
 
-            VSpace(30),
-
-            Expanded(
-              child: BlocConsumer<IncidentBloc, IncidentState>(
-                listener: (context, state) {
-                  if (state is IncidentErrorState) {
-                    CustomSnackbar.error(context, state.message);
-                  }
-                },
-                builder: (context, state) {
-                  if (state is IncidentLoadingState) {
-                    return Center(
-                      child: SpinKitThreeBounce(
-                        color: AppColors.scaffold,
-                        size: 30,
+                  IconButton.outlined(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        Colors.transparent,
                       ),
-                    );
-                  }
-
-                  if (state is GetIncidentsSuccessState) {
-                    return ListView.builder(
-                      itemCount: state.incidents.length,
-                      itemBuilder: (context, index) {
-                        final incident = state.incidents[index];
-
-                        return IncidentView(incident: incident);
-                      },
-                    );
-                  }
-
-                  return Center(
-                    child: Text(
-                      'No incident uploaded',
-                      style: TextStyle(fontSize: sp(16)),
                     ),
-                  );
-                },
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: AppColors.scaffold,
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+
+              VSpace(30),
+
+              Expanded(
+                child: BlocConsumer<IncidentBloc, IncidentState>(
+                  listener: (context, state) {
+                    if (state is IncidentErrorState) {
+                      CustomSnackbar.error(context, state.message);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is IncidentLoadingState) {
+                      return Center(
+                        child: SpinKitThreeBounce(
+                          color: AppColors.scaffold,
+                          size: 30,
+                        ),
+                      );
+                    }
+
+                    if (state is GetIncidentsSuccessState) {
+                      return ListView.builder(
+                        itemCount: state.incidents.length,
+                        itemBuilder: (context, index) {
+                          final incident = state.incidents[index];
+
+                          return IncidentView(incident: incident);
+                        },
+                      );
+                    }
+
+                    return Center(
+                      child: Text(
+                        'No incident uploaded',
+                        style: TextStyle(fontSize: sp(16)),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/incidents/presentation/widgets/incident_view.dart
+++ lib/features/incidents/presentation/widgets/incident_view.dart
@@ -1,3 +1,7 @@
+import 'package:citizen_report_incident/core/common/theme/app_colors.dart';
+import 'package:citizen_report_incident/core/utils/fomat_date.dart';
+import 'package:citizen_report_incident/core/utils/screen_util.dart';
+
 import '../../domain/entities/incident_entity.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +12,52 @@ class IncidentView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(incident.title);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Chip(
+          label: Text(incident.category),
+          color: WidgetStatePropertyAll(AppColors.scaffold),
+          labelStyle: TextStyle(color: AppColors.white),
+        ),
+        VSpace(10),
+        Text(
+          incident.title,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: sp(22)),
+        ),
+
+        VSpace(10),
+
+        Text(
+          'UploadedBy: ${incident.createdByUsername}',
+          style: TextStyle(fontSize: sp(16), fontWeight: FontWeight.w500),
+        ),
+
+        VSpace(5),
+
+        Text(
+          formatDateBydMMYYYY(incident.createdAt),
+          style: TextStyle(fontSize: sp(16), fontWeight: FontWeight.w500),
+        ),
+
+        VSpace(20),
+
+        ClipRRect(
+          borderRadius: BorderRadius.circular(sr(10)),
+          child: Image.network(incident.imageUrl),
+        ),
+
+        VSpace(20),
+
+        Text(
+          incident.description,
+          style: TextStyle(
+            fontSize: sp(16),
+            fontWeight: FontWeight.w500,
+            height: 2,
+          ),
+        ),
+      ],
+    );
   }
 }
